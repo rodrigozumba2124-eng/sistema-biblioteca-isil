d85d72212803f06fe2d4df046e3319691298ccb1
Rodrigo Zumba <tu_correo_de_github@gmail.com>
2026-06-07 18:25:23 -0500

    Corrección final de modelos y controladores

--- app/Http/Controllers/Api/AutorController.php
+++ app/Http/Controllers/Api/AutorController.php
@@ -1,7 +1,21 @@
-public function index() {
-    return \App\Models\Autor::all();
-}
+<?php
 
-public function store(\Illuminate\Http\Request $request) {
-    return \App\Models\Autor::create($request->all());
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Autor;
+use Illuminate\Http\Request;
+
+class AutorController extends Controller
+{
+    public function index()
+    {
+        return Autor::all();
+    }
+
+    public function store(Request $request)
+    {
+        $autor = Autor::create($request->all());
+        return response()->json($autor, 201);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AutorController;
+use App\Http\Controllers\Api\LibroController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Rutas para Autores
+Route::get('/api/autores', [AutorController::class, 'index']);
+Route::post('/api/autores', [AutorController::class, 'store']);
+
+// Rutas para Libros
+Route::get('/api/libros', [LibroController::class, 'index']);
+Route::post('/api/libros', [LibroController::class, 'store']);
